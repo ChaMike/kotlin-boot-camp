cff409b3691da075da15bce75c144938cdedd3e4
Michael Chaschin <chamike12@gmail.com>
2018-10-07 23:52:07 +0300

    02

--- assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
+++ assignments/02/src/main/kotlin/io/rybalkinsd/kotlinbootcamp/geometry/Geometry.kt
@@ -6,13 +6,21 @@ package io.rybalkinsd.kotlinbootcamp.geometry
 interface Collider {
     fun isColliding(other: Collider): Boolean
 }
-
 /**
  * 2D point with integer coordinates
  */
-class Point(x: Int, y: Int) : Collider {
+class Point(val x: Int, val y: Int) : Collider {
+
+    override fun equals(other: Any?): Boolean {
+         return (other is Point) && (this.x == other.x) && (this.y == other.y)
+    }
+
     override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+        return when (other) {
+            is Point -> this.equals(other)
+            is Bar -> other.isColliding(this)
+            else -> false
+        }
     }
 }
 
@@ -22,8 +30,27 @@ class Point(x: Int, y: Int) : Collider {
  * Bar is not oriented
  * (It does not matter, which opposite corners you choose to define bar)
  */
-class Bar(firstCornerX: Int, firstCornerY: Int, secondCornerX: Int, secondCornerY: Int) : Collider {
+class Bar(val firstCornerX: Int, val firstCornerY: Int, val secondCornerX: Int, val secondCornerY: Int) : Collider {
+
     override fun isColliding(other: Collider): Boolean {
-        TODO("not implemented")
+        return when (other) {
+            is Point -> PointCollidingBar(other)
+            is Bar -> BarCollidingBar(other)
+            else -> false
+        }
+    }
+
+    fun PointCollidingBar(other: Point): Boolean {
+        return if ((other.x <= maxOf(this.firstCornerX, this.secondCornerX))&&(other.x >= minOf(this.firstCornerX, this.secondCornerX))&&(other.y <= maxOf(this.firstCornerY, this.secondCornerY))&&(other.y >= minOf(this.firstCornerY, this.secondCornerY))) true else false
+    }
+
+    fun BarCollidingBar(other: Bar): Boolean {
+        var ur = false
+        val p1 = Point(other.firstCornerX, other.firstCornerY)
+        val p2 = Point(other.firstCornerX, other.secondCornerY)
+        val p3 = Point(other.secondCornerX, other.firstCornerY)
+        val p4 = Point(other.secondCornerX, other.secondCornerY)
+        if ((PointCollidingBar(p1))||(PointCollidingBar(p2))||(PointCollidingBar(p3))||(PointCollidingBar(p4))) ur = true else ur = false
+        return ur
     }
 }
